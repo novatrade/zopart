a0fe4469de1150931e6ea26157c1d143c7e28eab
Giuseppe Di Natale <gdinatale@novatradesrl.it>
2013-08-02 09:33:35 +0000

    Initialization

---
+++ 0.1/index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <style>
+         body       {margin:0; padding:0}
+         #Container {margin:0; padding:0; width:100%; height:100%}
+         #Header    {width:100%; text-align:center; background:black; height:40px}
+         #Logo      {font-family: courier-new; color:white}
+      </style>
+   </head>
+   <body>
+      <div id="Container">
+         <div id="Header">
+            <span id="Logo">ZopArt</span>
+         </div>
+      </div>
+   </body>
+</html>
